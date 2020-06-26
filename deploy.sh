c11d9f9bb7dcc9d43210af337b05ab4c1176dd65
Shabeeb R P <shabeebrp@gmail.com>
2020-06-26 14:02:28 +0530

    add deploy file to deploy to github pages

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ cd dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
-
+git init
 git add -A
 git commit -m 'deploy'
 
@@ -19,6 +19,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:shabeebrp/medium.git master
+git push -f https://github.com/shabeebrp/medium.git master:gh-pages
 
 cd -
